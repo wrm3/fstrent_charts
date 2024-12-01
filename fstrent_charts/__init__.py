f344d97d10680265920cb006cdd011416ec5d944
wrm3 <wrmartel3@gmail.com>
2024-12-01 18:08:49 -0500

    Bump version: 1.0.3 → 1.0.4

--- fstrent_charts/__init__.py
+++ fstrent_charts/__init__.py
@@ -1,4 +1,4 @@
 """FSTrent Charts - Color Chart options for terminal output."""
 from .fstrent_charts import CHRT, chart_top, chart_title, chart_headers, chart_row, chart_bottom
 
-__version__ = "1.0.3"
+__version__ = "1.0.4"
